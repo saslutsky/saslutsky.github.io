37f36f4bb002098ed13ff38ec2df63e0e09cf750
Simon Slutsky <themightysalmon@gmail.com>
2020-07-20 16:28:16 -0700

    Upgraded fetchCurrentRatings to a function

--- USCFCurrentRatingsScraper.py
+++ USCFCurrentRatingsScraper.py
@@ -3,47 +3,51 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import sys
 
-# Create Base URL Pointing to member mainpage
-baseURL = 'https://www.uschess.org/msa/MbrDtlMain.php?'
-idNum = '12687356' 
-URLtoFetch = baseURL + idNum
-# print(URLtoFetch) Insert validation here
+#function to fetch current ratings 
+def fetchCurrentRatings(idNum):
+    # Create Base URL Pointing to member mainpage
+    baseURL = 'https://www.uschess.org/msa/MbrDtlMain.php?'
+    #idNum = '12687356' 
+    URLtoFetch = baseURL + idNum
+    # print(URLtoFetch) Insert validation here
+    
+    # Fetch Soup
+    reqHTML = requests.get(URLtoFetch)
+    soup = BeautifulSoup(reqHTML.text, 'lxml')
+    
+    # Tables are too nested for soup to find one containing just the ratings
+    #ratingTable = soup.find_all('table')[2]
+    
+    # Extract rows of the webpage that contain the Rating Data. Can find directly from the soup
+    ratingRows  = soup.find_all('tr')
+    
+    current_df = pd.DataFrame(columns=["Rating"], index=["Regular", 
+                                                         "Quick", 
+                                                         "Blitz",
+                                                         "Online-Regular",
+                                                         "Online-Quick",
+                                                         "Online-Blitz",
+                                                         "Correspondence"]) #FIDE ?
 
-# Fetch Soup
-reqHTML = requests.get(URLtoFetch)
-soup = BeautifulSoup(reqHTML.text, 'lxml')
+    #X#X#X#X#X#X#X Find the column with the relevant rating info
+    #Fill a pandas dataFrame with the interesting ratings
+    for rrow in ratingRows:
+        rcolumns = rrow.find_all('td')  #Many rows found are bizarre but eventually the ratings are found
+        #    for column in rcolumns:
+        #       print (column.get_text())
+        print ("*************************")
+        print (rcolumns[0].get_text())
+        if (len(rcolumns) > 1):
+            print (rcolumns[1].get_text())
+            for ind in current_df.index:
+                if (rcolumns[0].get_text(strip=True) == ind + " Rating"):
+                    print ("W00t")
+                    current_df.at[ind, "Rating"] = rcolumns[1].get_text(strip=True)
 
-# Tables are too nested for soup to find one containing just the ratings
-#ratingTable = soup.find_all('table')[2]
-
-# Extract rows of the webpage that contain the Rating Data. Can find directly from the soup
-ratingRows  = soup.find_all('tr')
-
-current_df = pd.DataFrame(columns=["Rating"], index=["Regular", 
-                                                     "Quick", 
-                                                     "Blitz",
-                                                     "Online-Regular",
-                                                     "Online-Quick",
-                                                     "Online-Blitz",
-                                                     "Correspondence"]) #FIDE ?
-
-#X#X#X#X#X#X#X Find the column with the relevant rating info
-#Fill a pandas dataFrame with the interesting ratings
-for rrow in ratingRows:
-    rcolumns = rrow.find_all('td')  #Many rows found are bizarre but eventually the ratings are found
-#    for column in rcolumns:
- #       print (column.get_text())
-    print ("*************************")
-    print (rcolumns[0].get_text())
-    if (len(rcolumns) > 1):
-        print (rcolumns[1].get_text())
-    for ind in current_df.index:
-        if (rcolumns[0].get_text(strip=True) == ind + " Rating"):
-            print ("W00t")
-            current_df.at[ind, "Rating"] = rcolumns[1].get_text(strip=True)
-
-print (current_df)
+    print (current_df)
 
 
-
+if __name__ == "__main__":
+    fetchCurrentRatings(sys.argv[1])
