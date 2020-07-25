b8a466504b12ed2e87e16090b0c60a0875b8673f
Simon Slutsky <themightysalmon@gmail.com>
2020-07-25 00:38:52 -0700

    Working on parsing rating history HTML. getCells(uscfID) works on a test page

--- USCFRatingHistoryScraper.py
+++ USCFRatingHistoryScraper.py
@@ -7,11 +7,45 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import sys
 
+def hasArrow(instring):
+    if (instring.find("=>") > -1):
+        return 1
+    else:
+        return 0
+
+#Aux function to get cells of a row of a soup and return a dataframe
+def getCells(asoup):
+    adf = pd.DataFrame(columns=["Date/ID",
+                                "Event",
+                                "Regular",
+                                "Quick",
+                                "Blitz"]  ,
+                       index=range(0, 50)) #50 entries per page 
+
+
+    ratecount = 0
+    rows = asoup.find_all('tr')
+    for row in rows:
+        hasRating = 0
+        cells = row.find_all('td')
+        for cell in cells:
+            if hasArrow(cell.get_text()):
+                hasRating = 1
+        if (hasRating == 1 and len(cells) < 6):
+            for i in range(0, len(cells)):
+                adf.at[ratecount, adf.columns[i]] = cells[i].get_text()
+            ratecount += 1
+
+    return adf
+                       ######################### To Finish
+
+#Auxiliary function to extract rating from table entry
+#def grabRating(
+
 #function to fetch current ratings 
 def fetchRatingHistory(idNum):
     # Create Base URL Pointing to member rating history
     baseURL = 'https://www.uschess.org/msa/MbrDtlTnmtHst.php?'
-    #idNum = '12687356' 
     URLtoFetch = baseURL + idNum
     # print(URLtoFetch) Insert validation here
     
@@ -35,6 +69,18 @@ def fetchRatingHistory(idNum):
         littleURL = URLtoFetch + "." + str(page)
         littleHTML = requests.get(littleURL)
         souplist.append(BeautifulSoup(littleHTML.text, 'lxml'))
+    
+        #test
+    testee = getCells(souplist[0])
+    print (testee)
+    return testee
+
+    #return souplist
+    #for i in range(0, nPgHst):
+    #    print (souplist[i])
+
+    #return -1
+
     
 
     ######### Everything below here needs to be replaced
